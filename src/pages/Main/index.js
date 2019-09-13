1713be207186e3c1af289eb6151775ab9ea5aef8
Frederiko C. M. Ribeiro <frederiko.cmr@gmail.com>
2019-09-13 00:37:49 -0300

    Added Axios and API access

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,26 +1,63 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import api from '../../services/api';
 import { Container, SubmitButton, Form, Input } from './styles';
 
-// import { Container } from './styles';
+export default class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newUser: '',
+      users: [],
+    };
+  }
 
-export default function Main() {
-  return (
-    <Container>
-      <Form>
-        <Input
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeholder="Adicionar usuário"
-        />
-        <SubmitButton>
-          <Icon name="add" size={20} color="#FFF" />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+  handleAddUser = async () => {
+    const { users, newUser } = this.state;
+
+    const response = await api.get(`/users/${newUser}`);
+
+    const { name, login, bio, avatar_url: avatar } = response.data;
+
+    const data = {
+      name,
+      login,
+      bio,
+      avatar,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+    });
+
+    Keyboard.dismiss();
+  };
+
+  render() {
+    const { newUser } = this.state;
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton onPress={this.handleAddUser}>
+            <Icon name="add" size={20} color="#FFF" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 Main.navigationOptions = {
