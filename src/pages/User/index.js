84aa3fe43f1febd67ea232127f711897465f4241
Frederiko C. M. Ribeiro <frederiko.cmr@gmail.com>
2019-09-14 01:29:06 -0300

    Added styles to User page

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
 
 import api from '../../services/api';
+
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
 const propTypes = {
   navigation: PropTypes.shape({
@@ -32,7 +45,34 @@ class User extends Component {
   }
 
   render() {
-    return <View />;
+    const { navigation } = this.props;
+    const { users, stars } = this.state;
+
+    const user = navigation.getParam('user');
+
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      </Container>
+    );
   }
 }
 
