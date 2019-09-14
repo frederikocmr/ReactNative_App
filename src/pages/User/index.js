925a8ee4a1fd164eb1837ddc96becf51abdb038e
Frederiko C. M. Ribeiro <frederiko.cmr@gmail.com>
2019-09-14 00:59:18 -0300

    Fetching data from the Github API for the user starred repos

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,16 +1,41 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { View } from 'react-native';
 
-// import { Container } from './styles';
+import api from '../../services/api';
 
-export default function User() {
-  return (
-    <View>
-      <Text>Teste</Text>
-    </View>
-  );
+const propTypes = {
+  navigation: PropTypes.shape({
+    getParam: PropTypes.func,
+  }).isRequired,
+};
+
+class User extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('user').name,
+  });
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      stars: [],
+    };
+  }
+
+  async componentDidMount() {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+
+    const response = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({ stars: response.data });
+  }
+
+  render() {
+    return <View />;
+  }
 }
 
-User.navigationOptions = {
-  title: 'User',
-};
+User.propTypes = propTypes;
+
+export default User;
