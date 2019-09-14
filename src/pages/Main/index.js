51ca5775d3ca84cf18bfca58bc21624f8a0edb38
Frederiko C. M. Ribeiro <frederiko.cmr@gmail.com>
2019-09-14 00:35:57 -0300

    Added propTypes, Async Storage, styling to listing and navigation

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,21 +1,66 @@
 import React, { Component } from 'react';
-import { Keyboard } from 'react-native';
+import PropTypes from 'prop-types';
+import { Keyboard, ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import api from '../../services/api';
-import { Container, SubmitButton, Form, Input } from './styles';
+import {
+  Container,
+  SubmitButton,
+  Form,
+  Input,
+  List,
+  User,
+  Avatar,
+  Bio,
+  Name,
+  ProfileButton,
+  ProfileButtonText,
+} from './styles';
 
-export default class Main extends Component {
+const propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }).isRequired,
+};
+
+class Main extends Component {
+  static navigationOptions = {
+    title: 'Usuários',
+  };
+
   constructor(props) {
     super(props);
     this.state = {
       newUser: '',
       users: [],
+      loading: false,
     };
+  }
+
+  async componentDidMount() {
+    const users = await AsyncStorage.getItem('users');
+
+    if (users) {
+      this.setState({
+        users: JSON.parse(users),
+      });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { users } = this.state;
+
+    if (prevState.users !== users) {
+      AsyncStorage.setItem('users', JSON.stringify(users));
+    }
   }
 
   handleAddUser = async () => {
     const { users, newUser } = this.state;
+
+    this.setState({ loading: true });
 
     const response = await api.get(`/users/${newUser}`);
 
@@ -31,13 +76,20 @@ export default class Main extends Component {
     this.setState({
       users: [...users, data],
       newUser: '',
+      loading: false,
     });
 
     Keyboard.dismiss();
   };
 
+  handleNavigate = user => {
+    const { navigation } = this.props;
+
+    navigation.navigate('User', { user });
+  };
+
   render() {
-    const { newUser } = this.state;
+    const { newUser, users, loading } = this.state;
 
     return (
       <Container>
@@ -51,15 +103,39 @@ export default class Main extends Component {
             returnKeyType="send"
             onSubmitEditing={this.handleAddUser}
           />
-          <SubmitButton onPress={this.handleAddUser}>
-            <Icon name="add" size={20} color="#FFF" />
+          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+            {loading ? (
+              <ActivityIndicator color="#FFF" />
+            ) : (
+              <Icon name="add" size={20} color="#FFF" />
+            )}
           </SubmitButton>
         </Form>
+
+        <List
+          data={users}
+          keyExtractor={user => user.login}
+          renderItem={({ item }) => (
+            <User>
+              <Avatar source={{ uri: item.avatar }} />
+              <Name>{item.name}</Name>
+              <Bio>{item.bio}</Bio>
+
+              <ProfileButton
+                onPress={() => {
+                  this.handleNavigate(item);
+                }}
+              >
+                <ProfileButtonText>Ver Perfil</ProfileButtonText>
+              </ProfileButton>
+            </User>
+          )}
+        />
       </Container>
     );
   }
 }
 
-Main.navigationOptions = {
-  title: 'Usuários',
-};
+Main.propTypes = propTypes;
+
+export default Main;
